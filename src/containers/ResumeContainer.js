f1c80632d126bf90f8cd542884d045330821bf17
phil <phil.roth077@gmail.com>
2024-03-06 14:42:52 -0800

    dev to

--- src/containers/ResumeContainer.js
+++ src/containers/ResumeContainer.js
@@ -1,19 +1,24 @@
 /*eslint-disable */
 import React, { useContext } from "react"
+import { Document } from "react-pdf"
 import { Context } from "../context/Context"
 
 const ResumeContainer = () => {
-  const {
-    links: { resume },
-  } = useContext(Context)
+	const {
+		links: { resume },
+	} = useContext(Context)
 
-  // ? Resume px => 800 x 1035
-  const resumeStyle = { height: "1035px", width: "100%", overflow: "auto" }
-  return (
-    <div className='resume-container'>
-      <iframe id='resume-frame' src={resume} style={resumeStyle}></iframe>
-    </div>
-  )
+	// ? Resume px => 800 x 1035
+	const resumeStyle = { height: "1035px", width: "100%", overflow: "auto" }
+	if (true) {
+		return <Document file="../assets/Phil Roth Software Engineer Resume.pdf" />
+	} else {
+		return (
+			<div className="resume-container">
+				<iframe id="resume-frame" src={resume} style={resumeStyle}></iframe>
+			</div>
+		)
+	}
 }
 
 export default ResumeContainer
